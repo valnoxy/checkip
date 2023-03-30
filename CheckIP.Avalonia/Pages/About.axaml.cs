60e25116540df8fc727d01e00bf8096ecc7fe2f4
valnoxy <jonas@exploitox.de>
2023-03-30 22:25:44 +0200

    ⚒️ My IP functionality implemented

--- CheckIP.Avalonia/Pages/About.axaml.cs
+++ CheckIP.Avalonia/Pages/About.axaml.cs
@@ -2,8 +2,10 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Svg.Skia;
 using Svg.Skia;
+using System;
 using System.Diagnostics;
 using System.Reflection;
+using Avalonia;
 
 namespace CheckIP.Avalonia.Pages
 {
@@ -13,9 +15,24 @@ namespace CheckIP.Avalonia.Pages
         {
             InitializeComponent();
 
-            var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
+            var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location);
+            var avaloniaInformationalVersion = typeof(AvaloniaObject).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+            var avaloniaVersion = avaloniaInformationalVersion![..avaloniaInformationalVersion.IndexOf("+", StringComparison.Ordinal)];
+
+            var fluentAvaloniaVersion = typeof(FluentAvalonia.UI.Controls.NavigationView).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+
+            var newtonSoftInformationalVersion = typeof(Newtonsoft.Json.JsonConvert).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+            var newtonsoftVersion = newtonSoftInformationalVersion![..newtonSoftInformationalVersion.IndexOf("+", StringComparison.Ordinal)];
+
             ValueVersion.Content = $"{versionInfo.ProductName} V. {versionInfo.ProductVersion}";
             ValueCopyright.Content = versionInfo.LegalCopyright;
+            AvaloniaVersion.Content = $"Avalonia V. {avaloniaVersion}";
+            FluentAvaloniaVersion.Content = $"FluentAvalonia V. {fluentAvaloniaVersion}";
+            NewtonsoftVersion.Content = $"Newtonsoft.Json V. {newtonsoftVersion}";
+
         }
     }
 }
--- CheckIP.Avalonia/Pages/MyIP.axaml.cs
+++ CheckIP.Avalonia/Pages/MyIP.axaml.cs
@@ -1,13 +1,170 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using System;
+using System.IO;
 
 namespace CheckIP.Avalonia.Pages
 {
     public partial class MyIP : UserControl
     {
+        private static string _myIp;
+
         public MyIP()
         {
             InitializeComponent();
+            _myIp = Task.Run(_GetIPAddress).GetAwaiter().GetResult();
+            Task.Run(ParseIpAddress);
+        }
+
+        [Obsolete("Will be replaced to a async version soon.")]
+        private static async Task<string> _GetIPAddress()
+        {
+            string result = null;
+            try
+            {
+                result = await Task.Run(() => new WebClient().DownloadString("https://ifconfig.me/ip"));
+            }
+            catch
+            {
+                try
+                {
+                    result = await Task.Run(() => new WebClient().DownloadString("https://api.ipify.org"));
+                }
+                catch
+                {
+
+                }
+            }
+
+            return result;
+        }
+
+        private void ParseIpAddress()
+        {
+            // Validate IP
+            var validateIp = IPAddress.TryParse(_myIp, out var ip);
+            if (!validateIp)
+            {
+                ErrorLabel.Content = "Error: This is not a valid IP address";
+                return;
+            }
+
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                ErrorLabel.Content = string.Empty;
+            });
+
+            var dataJson = string.Empty;
+            var url = "http://ip-api.com/json/" + _myIp + "?fields=status,message,country,countryCode,city,zip,lat,lon,timezone,isp,as,mobile,proxy,hosting";
+            try
+            {
+                var client = new HttpClient();
+                using var response = client.GetAsync(url).Result;
+                using var content = response.Content;
+                dataJson = content.ReadAsStringAsync().Result;
+            }
+            catch
+            {
+                Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    ErrorLabel.Content = "Error: No connection to server";
+                });
+            }
+            dynamic data = JObject.Parse(dataJson);
+
+            // Parse data
+            string status = data.status;
+            string message = data.message;
+            string country = data.country;
+            string countryCode = data.countryCode;
+            string city = data.city;
+            string postal = data.zip;
+            string timezone = data.timezone;
+            string latitude = data.lat;
+            string longitude = data.lon;
+            string isp = data.isp;
+            string asn = data.@as;
+            string mobile = data.mobile;
+            string proxy = data.proxy;
+            string hosting = data.hosting;
+
+            // Check status
+            if (status != "success")
+            {
+                Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    ErrorLabel.Content = "Error: " + message;
+                    IpAddress.Content = "Unknown";
+                    ValueCityCountry.Text = "Unknown";
+                    ValuePostal.Text = "Unknown";
+                    ValueTimezone.Text = "Unknown";
+                    ValueLatitude.Text = "Unknown";
+                    ValueLongitude.Text = "Unknown";
+                    ValueIsp.Text = "Unknown";
+                    ValueAsn.Text = "Unknown";
+                    ValueMobile.Text = "Unknown";
+                    ValueProxy.Text = "Unknown";
+                    ValueHosting.Text = "Unknown";
+                });
+                return;
+            }
+
+            // Set Variable labels
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                IpAddress.Content = _myIp;
+                ValueCityCountry.Text = city + " / " + country + " (" + countryCode + ")";
+                ValuePostal.Text = postal;
+                ValueTimezone.Text = timezone;
+                ValueLatitude.Text = latitude;
+                ValueLongitude.Text = longitude;
+                ValueIsp.Text = isp;
+                ValueAsn.Text = asn;
+                ValueMobile.Text = mobile;
+                ValueProxy.Text = proxy;
+                ValueHosting.Text = hosting;
+            });
+        }
+
+        private void ExportBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Build export string
+            var exportString = $@"Report created at {DateTime.Now} for IP {IpAddress.Content}
+City / Country: {ValueCityCountry.Text}
+Postal: {ValuePostal.Text}
+Timezone: {ValueTimezone.Text}
+Latitude: {ValueLatitude.Text}
+Longitude: {ValueLongitude.Text}
+ISP or Organization: {ValueIsp.Text}
+ASN: {ValueAsn.Text}
+Is Mobile: {ValueMobile.Text}
+Is Proxy: {ValueProxy.Text}
+Is Hosting: {ValueHosting.Text}
+";
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filters =
+                {
+                    new FileDialogFilter
+                    {
+                        Name = "Text file",
+                        Extensions = new List<string> {"txt"}
+                    }
+                },
+                InitialFileName = "CheckIP_Report.txt"
+            };
+            var SettingsFileName = saveFileDialog.ShowAsync;
+
+            if (SettingsFileName != null)
+                File.WriteAllText(SettingsFileName.ToString(), exportString);
         }
     }
 }
