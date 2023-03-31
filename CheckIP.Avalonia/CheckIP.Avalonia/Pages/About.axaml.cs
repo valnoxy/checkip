1be96063f85ebb41adde72ddf0ddf5f74ab49d9c
Jonas Günner <jonas@exploitox.de>
2023-03-31 03:27:22 +0200

    Finalized release build script

--- CheckIP.Avalonia/CheckIP.Avalonia/Pages/About.axaml.cs
+++ CheckIP.Avalonia/CheckIP.Avalonia/Pages/About.axaml.cs
@@ -1,9 +1,5 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Svg.Skia;
-using Svg.Skia;
 using System;
-using System.Diagnostics;
 using System.Reflection;
 using Avalonia;
 
@@ -15,24 +11,64 @@ namespace CheckIP.Pages
         {
             InitializeComponent();
 
-            var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location);
-            var avaloniaInformationalVersion = typeof(AvaloniaObject).Assembly
-                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
-            var avaloniaVersion = avaloniaInformationalVersion![..avaloniaInformationalVersion.IndexOf("+", StringComparison.Ordinal)];
+            try
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                var productName = Assembly.GetExecutingAssembly().GetName().Name;
+                ValueVersion.Content = $"{productName} V. {version}";
+                ValueCopyright.Content = GetCopyright();
+                
+                var avaloniaInformationalVersion = typeof(AvaloniaObject).Assembly
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+                var avaloniaVersion =
+                    avaloniaInformationalVersion![
+                        ..avaloniaInformationalVersion.IndexOf("+", StringComparison.Ordinal)];
 
-            var fluentAvaloniaVersion = typeof(FluentAvalonia.UI.Controls.NavigationView).Assembly
-                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+                var fluentAvaloniaVersion = typeof(FluentAvalonia.UI.Controls.NavigationView).Assembly
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
 
-            var newtonSoftInformationalVersion = typeof(Newtonsoft.Json.JsonConvert).Assembly
-                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
-            var newtonsoftVersion = newtonSoftInformationalVersion![..newtonSoftInformationalVersion.IndexOf("+", StringComparison.Ordinal)];
+                var newtonSoftInformationalVersion = typeof(Newtonsoft.Json.JsonConvert).Assembly
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+                var newtonsoftVersion =
+                    newtonSoftInformationalVersion![
+                        ..newtonSoftInformationalVersion.IndexOf("+", StringComparison.Ordinal)];
 
-            ValueVersion.Content = $"{versionInfo.ProductName} V. {versionInfo.ProductVersion}";
-            ValueCopyright.Content = versionInfo.LegalCopyright;
-            AvaloniaVersion.Content = $"Avalonia V. {avaloniaVersion}";
-            FluentAvaloniaVersion.Content = $"FluentAvalonia V. {fluentAvaloniaVersion}";
-            NewtonsoftVersion.Content = $"Newtonsoft.Json V. {newtonsoftVersion}";
-
+                AvaloniaVersion.Content = $"Avalonia V. {avaloniaVersion}";
+                FluentAvaloniaVersion.Content = $"FluentAvalonia V. {fluentAvaloniaVersion}";
+                NewtonsoftVersion.Content = $"Newtonsoft.Json V. {newtonsoftVersion}";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        
+        private string GetCopyright()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var obj = asm.GetCustomAttributes(false);
+            foreach (var o in obj)
+            {
+                if (o.GetType() !=
+                    typeof(System.Reflection.AssemblyCopyrightAttribute)) continue;
+                var aca = (AssemblyCopyrightAttribute) o;
+                return aca.Copyright;
+            }
+            return string.Empty;
+        }
+        
+        private string GetVersion()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var obj = asm.GetCustomAttributes(false);
+            foreach (var o in obj)
+            {
+                if (o.GetType() !=
+                    typeof(System.Reflection.AssemblyVersionAttribute)) continue;
+                var aca = (AssemblyVersionAttribute) o;
+                return aca.Version;
+            }
+            return string.Empty;
         }
     }
 }
